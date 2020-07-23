2434b26be105b5f586e1d29cd40eb0a8b9750b90
unknown <remi4sure1@gmail.comYushau Balogungit config --global user.email remi4sure1@gmail.com>
2020-07-23 08:58:02 -0700

    Initial Project Version

---
+++ variable.py
@@ -0,0 +1,19 @@
+x = 1
+y = 2.5
+name = 'john'
+# assignments: give five examples of variables names and assign value to them of different types.
+is_cool = True
+he_is_tall = True
+name = 'joshua'
+size = 15
+height = 6
+""" 
+this is 
+a python class
+where we will be learning how to code
+"""
+print (y)
+print (is_cool)
+print (x)
+favorite_color = ['white','grey', 'blue']
+print (favorite_color)
